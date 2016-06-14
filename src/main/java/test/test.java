885cd5d4987e88c5efb1ec0141b12c92ae8d5892
Sumit Ghosh <sumit_4ever@rediffmail.com>
2016-06-14 17:36:18 +0530

    changes

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -3,6 +3,7 @@ import DriverUtilities.WebDriverConfigure;
 
 public class test extends WebDriverConfigure {
 	public boolean AssertTrueMethod(String s){
+		System.out.println(s);
 		 if(driver.getTitle().contains(s))
 			 return true;
 		 else
